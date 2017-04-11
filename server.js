21cd52c673490a482f7ea2f171872d81fb481fbb
glstream <grayson.stream@gmail.com>
2017-04-10 19:32:12 -0700

    added node porject

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.path}`
+    fs.appendFile('server.log', log+ '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintence.hbs', {
+        maintence: 'The webiste is under maintence'
+    });
+
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIT', (text) => {
+    return text.toUpperCase();
+})
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        welcome: 'Welcome to the homepage',
+        pageTitle: 'HomePage'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/bad', (req, res)=>{
+    res.send({
+
+        error: 'unable to handle'
+        });
+    
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
